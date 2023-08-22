d59aba08cc7b2e33b948ee3a55f32675b5ff78d9
srikishore5727 <srikishore9080676683@gmail.com>
2023-08-22 16:10:46 +0530

    project completed

---
+++ app.js
@@ -0,0 +1,8 @@
+
+    const bottom_btn=document.getElementById("bottom-btn")
+    const thanks_msg=document.getElementById("thanks-msg")
+
+    bottom_btn.onclick=()=>{
+        thanks_msg.style.display="block";
+        bottom_btn.style.display="none";
+    }
